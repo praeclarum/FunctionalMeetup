4b9d9be59cb587e65ec7d8bc611dd53c5e69eff5
Frank A. Krueger <fak@praeclarum.org>
2016-06-03 17:41:40 -0700

    Add CreateMeetup Command

---
+++ FunctionalMeetup.Core/UserCommands.fs
@@ -0,0 +1,34 @@
+﻿namespace FunctionalMeetup
+
+open System
+
+type Validation =
+    | Valid
+    | Invalid of string
+    member this.ThrowIfInvalid () =
+        match this with
+        | Invalid m -> raise (new Exception (m))
+        | _ -> ()
+        
+type UserCommands (client : IMeetupClient, repo:AppRepo) =
+    
+    member this.ValidateCreateMeetup (name : string) =
+        if String.IsNullOrWhiteSpace (name) then
+            Invalid "Name must be given"
+        else Valid
+    
+    member this.CreateMeetupAsync (userId:string)
+                                  (name:string) (loc:Location) =
+        async {
+            let v = this.ValidateCreateMeetup name
+            v.ThrowIfInvalid ()
+            
+            let! serverMeetup = client.CreateMeetupAsync userId name loc
+            
+            repo.UpdateData (fun data ->
+                let newMeetups = Array.append [|serverMeetup|]
+                                              data.Meetups
+                Some { data with Meetups = newMeetups }) 
+        }                
+
+
